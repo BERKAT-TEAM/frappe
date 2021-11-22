6bb204565b8daebca36f5af9a7a5ce79f2ff9e26
berkat28 <bersamakreatifteknologi.otp@gmail.com>
2021-11-22 08:30:43 +0100

    new update from development PC2--Julia

---
+++ frappe/creative/report/kreatif/kreatif.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2013, Frappe Technologies and contributors
+# License: MIT. See LICENSE
+
+# import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
